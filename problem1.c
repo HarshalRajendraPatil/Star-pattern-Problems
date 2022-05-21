4c3f23e73af8ffc81999e2da7fa1274215c26ede
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2022-05-21 15:39:59 +0530

    Update problem1.c

--- problem1.c
+++ problem1.c
@@ -1,4 +1,4 @@
-// Printing the square with stars, where the number of rows for the squaer will be enter by the user.
+// Q.1 Printing the square with stars, where the number of rows for the squaer will be enter by the user.
 // EX.  ****
 //      ****
 //      ****
@@ -20,4 +20,27 @@ int main(){
     
 
     return 0;
+}
+
+
+Q.2 // Printing the rectangle with stars, where length and breadth are entered by the user.
+Ex. *******
+    *******
+    *******
+
+# include<stdio.h>
+
+int main(){
+    int length, breadth;
+    printf("What is the length of rectangle:\n");
+    scanf("%d", &length);
+    printf("What is the breadth of rectangle:\n");
+    scanf("%d", &breadth);
+
+    for(int i = 0; i < length; i++){
+        for (int j = 0; j < breadth; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 }
